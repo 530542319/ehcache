909109ed4b2d3735ac318e232e29b1edee9f117b
530542319 <1622059450@qq.com>
2016-05-27 17:01:40 +0800

    updated tow methods

--- EhCacheExample/src/main/java/com/toptal/blog/service/MessageService.java
+++ EhCacheExample/src/main/java/com/toptal/blog/service/MessageService.java
@@ -4,6 +4,10 @@ import java.util.concurrent.ConcurrentHashMap;
 import org.springframework.stereotype.Service;
 
 import com.googlecode.ehcache.annotations.Cacheable;
+import com.googlecode.ehcache.annotations.KeyGenerator;
+import com.googlecode.ehcache.annotations.PartialCacheKey;
+import com.googlecode.ehcache.annotations.Property;
+import com.googlecode.ehcache.annotations.TriggersRemove;
 
 @Service
 public class MessageService{
@@ -11,13 +15,13 @@ public class MessageService{
 	= new ConcurrentHashMap<Integer, String>();
 	
 	// 这两个方法未使用缓存
-	public String getMessage(Integer id) {
-		System.out.println("Getting data from SOR......");
-		return messages.get(id);
-	}
-	public void setMessage (Integer id, String message){
-		messages.put(id, message);
-	}
+//	public String getMessage(Integer id) {
+//		System.out.println("Getting data from SOR......");
+//		return messages.get(id);
+//	}
+//	public void setMessage (Integer id, String message){
+//		messages.put(id, message);
+//	}
 	
 	// 这两个方法使用缓存
 //	@Cacheable(cacheName = "messageCache", selfPopulating = true)
@@ -32,20 +36,20 @@ public class MessageService{
 
 	
 	// 这两个方法使用缓存 并且当数据有变化时立即刷新缓存数据
-//@Cacheable(cacheName = "messageCache",selfPopulating = true,//use self populating cache
-//		 keyGenerator = @KeyGenerator (//method name is not included in cache key to work with @TriggersRemove
-//	                name = "HashCodeCacheKeyGenerator",
-//	                properties = @Property( name="includeMethod", value="false" )))	
-//public String getMessage(Integer id) {
-//	System.out.println("Getting data from SOR......");
-//	return messages.get(id);
-//}
-//
-//@TriggersRemove(cacheName = "messageCache",
-//		 keyGenerator = @KeyGenerator (
-//	                name = "HashCodeCacheKeyGenerator",
-//	                properties = @Property( name="includeMethod", value="false" )))
-//public void setMessage (@PartialCacheKey Integer id, String message){
-//	messages.put(id, message);
-//}
+@Cacheable(cacheName = "messageCache",selfPopulating = true,//use self populating cache
+		 keyGenerator = @KeyGenerator (//method name is not included in cache key to work with @TriggersRemove
+	                name = "HashCodeCacheKeyGenerator",
+	                properties = @Property( name="includeMethod", value="false" )))	
+public String getMessage(Integer id) {
+	System.out.println("Getting data from SOR......");
+	return messages.get(id);
+}
+
+@TriggersRemove(cacheName = "messageCache", 
+		 keyGenerator = @KeyGenerator (
+	                name = "HashCodeCacheKeyGenerator",
+	                properties = @Property( name="includeMethod", value="false" )))
+public void setMessage (@PartialCacheKey Integer id, String message){
+	messages.put(id, message);
+}
 }
